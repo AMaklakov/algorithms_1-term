eec2684572b0f6c5f97baa062f8079caaa53637e
A.Maklakov <artsiom.maklakou@softclub.by>
2018-11-23 23:41:41 +0300

    6 lab

---
+++ 6/Graph.ts
@@ -0,0 +1,222 @@
+import {copyArray, EMPTY_FIELD} from './util';
+import * as cTable from 'console.table';
+import {createClient} from 'http';
+
+export class Graph {
+	adjacencyMatrix: number[][];
+	incidenceList: number[][];
+	verticesTotal: number;
+	dijkstraDistance: number[];
+
+	constructor(verticesTotal?: number) {
+		this.verticesTotal = verticesTotal || 1;
+		this.initAdjacencyMatrix();
+		this.initAdjacencyList();
+	}
+
+	// region EDGES and VERTICES
+	addVertex() {
+		this.verticesTotal++;
+		this.adjacencyMatrix.push(this.adjacencyMatrix[0].map(() => EMPTY_FIELD));
+		this.adjacencyMatrix.forEach(item => item.push(EMPTY_FIELD))
+	}
+
+	removeVertex(vertex: number = 0) {
+		if (!this.checkVertex(vertex)) {
+			console.log('remove vertex -> Not valid vertex', vertex);
+		}
+
+		this.verticesTotal--;
+		this.adjacencyMatrix = this.adjacencyMatrix
+			.map(item => item.splice(vertex, 1))
+			.splice(vertex, 1);
+	}
+
+	addEdge(vertex1: number, vertex2: number, weight?: number) {
+		this.addOneWayEdge(vertex1, vertex2, weight);
+		this.addOneWayEdge(vertex2, vertex1, weight);
+	}
+
+	addOneWayEdge(vertex1: number, vertex2: number, weight: number) {
+		if (!this.checkVertices([vertex1, vertex2])) {
+			return console.log('addEdge -> vertex1 or vertex2 is out of range');
+		}
+
+		if (vertex1 === vertex2) {
+			return console.log('Vertices are the same. Can\'t do that');
+		}
+
+		// greater than zero
+		weight = this.checkWeight(weight) ? weight : 1;
+
+		if (this.adjacencyMatrix[vertex1][vertex2] === EMPTY_FIELD) {
+			this.adjacencyMatrix[vertex1][vertex2] = weight;
+		} else {
+			console.log('addEdge -> edge is already exists');
+		}
+	}
+
+	removeEdge(vertex1: number, vertex2: number) {
+		this.removeOneWayEdge(vertex1, vertex2);
+		this.removeOneWayEdge(vertex2, vertex1);
+	}
+
+	removeOneWayEdge(vertex1: number, vertex2: number) {
+		if (!this.checkVertices([vertex1, vertex2])) {
+			return console.log('addEdge -> vertex1 or vertex2 is out of range');
+		}
+
+		if (vertex1 === vertex2) {
+			return console.log('Vertices are the same. Can\'t do that');
+		}
+
+		if (this.adjacencyMatrix[vertex1][vertex2] !== EMPTY_FIELD) {
+			this.adjacencyMatrix[vertex1][vertex2] = EMPTY_FIELD;
+		} else {
+			console.log('removeEdge -> no such side');
+		}
+	}
+
+	// endregion EDGES and VERTICES
+
+	dijkstra(start: number = 0) {
+		if (!this.checkVertex(start)) {
+			return console.log('dijkstra -> start is not valid');
+		}
+
+		// save matrix
+		const copiedMatrix: number[][] = copyArray(this.adjacencyMatrix);
+
+		// Array of ∞
+		const dist: number[] = this.adjacencyMatrix.map((item, index: number) => index !== start ? Infinity : 0);
+		const sptSet: boolean[] = this.adjacencyMatrix.map(() => false);
+
+		for (let i = 0; i < this.verticesTotal; i++) {
+			const u: number = this.findMinDistance(dist, sptSet);
+
+			sptSet[u] = true;
+
+			//for v in range(self.V):
+			//                 if self.graph[u][v] > 0 and sptSet[v] == False and
+			//                    dist[v] > dist[u] + self.graph[u][v]:
+			//                         dist[v] = dist[u] + self.graph[u][v]
+
+			for (let v = 0; v < this.verticesTotal; v++) {
+				if (this.adjacencyMatrix[u][v] !== EMPTY_FIELD && !sptSet[v] && dist[v] > dist[u] + this.adjacencyMatrix[u][v]) {
+					dist[v] = dist[u] + this.adjacencyMatrix[u][v];
+				}
+			}
+		}
+
+		// recover matrix
+		this.adjacencyMatrix = copiedMatrix;
+
+		// setting and printing result
+		this.dijkstraDistance = dist;
+
+
+		console.log(`distances from: ${start + 1} vertex`);
+		for (let i = 0; i < this.verticesTotal; i++) {
+			console.log(`${i} ->`, this.dijkstraDistance[i]);
+		}
+
+	}
+
+	floydWarshall() {
+		// adjacencyMatrix with 0 throughout main diagonal
+		const dist: number[][] = copyArray<number>(this.adjacencyMatrix)
+			.map((item, i) => item.map((num, j) => i !== j? num : 0));
+
+		for (let k = 0; k < this.verticesTotal; k++) {
+			for (let i = 0; i < this.verticesTotal; i++) {
+				for (let j = 0; j < this.verticesTotal; j++) {
+					// If vertex k is on the shortest path from
+					// i to j, then update the value of dist[i][j]
+					if (dist[i][k] + dist[k][j] < dist[i][j]) {
+						dist[i][j] = dist[i][k] + dist[k][j];
+					}
+				}
+			}
+		}
+
+		// print matrix
+		console.table(dist);
+	}
+
+	print() {
+		const arrayToPrint = this.adjacencyMatrix
+			.map((value: number[]) => value.map(item => item === EMPTY_FIELD ? '∞' : item));
+
+
+		console.log(`vertices: ${this.verticesTotal}, adjacencyMatrix:`);
+		console.log(cTable.getTable(arrayToPrint));
+		// console.table(this.adjacencyMatrix)
+	}
+
+	printIncidenceList() {
+		console.log(`vertices: ${this.verticesTotal}, incidenceList: \n`, this.incidenceList);
+	}
+
+	private initAdjacencyList() {
+		this.incidenceList = [];
+
+		this.incidenceList = this.adjacencyMatrix.map((item: number[]) => {
+			const arr = [];
+
+			item.forEach((num, index) => {
+				num && arr.push(index);
+			});
+
+			return arr;
+		});
+	}
+
+	private initAdjacencyMatrix() {
+		this.adjacencyMatrix = [];
+		for (let i = 0; i < this.verticesTotal; i++) {
+			this.adjacencyMatrix[i] = [];
+
+			for (let j = 0; j < this.verticesTotal; j++) {
+				this.adjacencyMatrix[i][j] = EMPTY_FIELD;
+			}
+		}
+
+		// this.adjacencyMatrix = new Array(this.verticesTotal).fill(0)
+	}
+
+	private checkVertices(vertices: number[]): boolean {
+		if (vertices) {
+			for (let vertex of vertices) {
+				if (!this.checkVertex(vertex)) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	private checkVertex(vertex: number): boolean {
+		return vertex >= 0 && vertex < this.verticesTotal;
+	}
+
+	private checkWeight(weight: number): boolean {
+		return weight > 0;
+	}
+
+	private findMinDistance(dist: number[], sptSet: boolean[]): number {
+		let min: number = Infinity;
+		let v: number = null;
+
+		for (let i = 0; i < this.verticesTotal; i++) {
+			if (dist[i] < min && !sptSet[i]) {
+				min = dist[i];
+				v = i;
+			}
+		}
+
+		return v;
+	}
+}
